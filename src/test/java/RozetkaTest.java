313a68439e71cd9c658b5afc1bb310a899050867
Trachova <y.trachova@gmail.com>
2017-06-01 21:09:58 +0300

    compare prices

--- src/test/java/RozetkaTest.java
+++ src/test/java/RozetkaTest.java
@@ -1,10 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by X230 on 01.06.2017.
@@ -27,10 +28,10 @@ public class RozetkaTest {
         driver.findElement(By.cssSelector("[name='text']")).sendKeys("TV");
         driver.findElement(By.cssSelector(".btn-link-i")).click();
         String actualPrice = driver.findElement(By.cssSelector("#js-product_670419-price")).getText();
-        Assert.assertEquals(actualPrice, expectedPrice);
+        assertEquals(actualPrice, expectedPrice);
         driver.findElement(By.cssSelector("#block_with_search > div > div:nth-child(6) > div > div > div > div > div.g-i-tile-i-title.clearfix > a")).click();
         String actualPrice2 = driver.findElement(By.cssSelector("#price_label")).getText();
-        Assert.assertEquals(actualPrice2, expectedPrice);
+        assertEquals(actualPrice2, expectedPrice);
     }
 
     @Test
