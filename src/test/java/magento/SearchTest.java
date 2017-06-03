371d26a4d247675d77ad6bbc97631732ce82e959
ruslan.bilyi <beliysacred@gmail.com>
2017-06-03 11:56:24 +0300

    reformatting

--- src/test/java/magento/SearchTest.java
+++ src/test/java/magento/SearchTest.java
@@ -43,7 +43,7 @@ public class SearchTest extends DriverConfiguration {
     }
 
     @Test
-    public void searchvase() throws InterruptedException {
+    public void searchVase() throws InterruptedException {
         driver.findElement(By.cssSelector("#search")).clear();
         driver.findElement(By.cssSelector("#search")).sendKeys("vase");
         driver.findElement(By.cssSelector(".button.search-button")).click();
@@ -52,7 +52,7 @@ public class SearchTest extends DriverConfiguration {
     }
 
     @Test
-    public void searchvase1() throws InterruptedException {
+    public void searchVasePOP() throws InterruptedException {
         HomePage homePage = new HomePage(driver);
         homePage.fillInSearchInputField("vase");
         homePage.clickOnSearchIcon();
