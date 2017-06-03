c6de1fd979d7b3436659ad171eb0164ac043ac78
ruslan.bilyi <beliysacred@gmail.com>
2017-06-03 12:30:02 +0300

    reformatting URL

--- src/test/java/magento/PasswordReset.java
+++ src/test/java/magento/PasswordReset.java
@@ -8,7 +8,7 @@ import preparation.DriverConfiguration;
  * Created by gryzyuka on 03.06.2017.
  */
 public class PasswordReset extends DriverConfiguration {
-    final String basicURL = "http://magento.brainacad.com/english/customer/account/login/";
+    final String basicURL = "http://magento.brainacad.com";
 
     @BeforeMethod
     public void beforeMethod() {
