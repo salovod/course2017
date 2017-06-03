2d7e482cc0f77f951db07055e4497b6a2104c9b8
Trachova <y.trachova@gmail.com>
2017-06-03 12:23:19 +0300

    login page

---
+++ src/main/java/magento/LoginPage.java
@@ -0,0 +1,14 @@
+package magento;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by X230 on 03.06.2017.
+ */
+public class LoginPage {
+    final String
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
