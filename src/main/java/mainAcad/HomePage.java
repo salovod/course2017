748d64f0a2a0c45d357791f3a430e26c5103731c
Trachova <y.trachova@gmail.com>
2017-06-06 20:53:40 +0300

    Home page main

--- src/main/java/mainAcad/HomePage.java
+++ src/main/java/mainAcad/HomePage.java
@@ -1,7 +1,22 @@
 package mainAcad;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by X230 on 06.06.2017.
  */
 public class HomePage {
+    final String courses = "#menu-item-10452";
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void courses() {
+        driver.findElement(By.cssSelector(courses)).click();
+    }
+}
+
 }
