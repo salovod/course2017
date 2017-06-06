c3f12f95c5ff7dab07a46e643437f102f6a439bf
Your Namegryzyuka <gryzyuka@gmail.com>
2017-06-06 20:49:29 +0300

    PortfolioCourses

---
+++ src/main/java/mainAcad/PortfolioCoursesPage.java
@@ -0,0 +1,16 @@
+package mainAcad;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by gryzyuka on 06.06.2017.
+ */
+public class PortfolioCoursesPage {
+    private final WebDriver driver;
+
+    public PortfolioCoursesPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
