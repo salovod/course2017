c77f5ef072b168a859bb02865220bff30b3efbc6
ruslan.bilyi <beliysacred@gmail.com>
2017-06-06 21:12:04 +0300

    reformatting

--- src/main/java/mainAcad/HomePage.java
+++ src/main/java/mainAcad/HomePage.java
@@ -8,22 +8,13 @@ import org.openqa.selenium.WebDriver;
  */
 public class HomePage {
     final String courses = "#menu-item-10452";
-    final String basic = "#post-9450";
-    final String getTeachText = "#forWho > div.container > div > div:nth-child(1) > p"
     private final WebDriver driver;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void courses() {
+    public void chooseCourse() {
         driver.findElement(By.cssSelector(courses)).click();
     }
-
-    public void basic() {
-        driver.findElement(By.cssSelector(basic)).click();
-    }
-
-    public String getTeachText() {
-        return driver.findElement(By.cssSelector(getTeachText)).getText();
 }
--- src/test/java/mainAcad/MainAcad.java
+++ src/test/java/mainAcad/MainAcad.java
@@ -25,7 +25,7 @@ public class MainAcad extends DriverConfiguration {
     public void frontEnd() throws InterruptedException {
         driver.findElement(By.cssSelector("#menu-item-10452")).click();
         PortfolioCoursesPage portfolioCoursesPage = new PortfolioCoursesPage(driver);
-        driver.findElement(By.cssSelector(portfolioCoursesPage.clickOnCourses("9018"))).click();
+        portfolioCoursesPage.clickOnCourses(9018);
         Assert.assertTrue(driver.findElement(By.cssSelector("#shortReiew")).isDisplayed());
         Assert.assertEquals("5 месяцев", driver.findElement(By.cssSelector("#shortReiew > div > div > div:nth-child(1) > div >p.sub")).getText());
         Assert.assertEquals("144 часа", driver.findElement(By.cssSelector("#shortReiew > div > div > div:nth-child(2) > div >p.sub")).getText());
@@ -51,6 +51,10 @@ public class MainAcad extends DriverConfiguration {
 
     @Test
     public void basic() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.chooseCourse();
+        PortfolioCoursesPage portfolioCoursesPage = new PortfolioCoursesPage(driver);
+        portfolioCoursesPage.clickOnCourses(9450);
         String expectedText = "Колеблешься, ту ли специальность выбрал? Уверен в правильности выбора, но хочешь усилить свои позиции техническими знаниями?\nПредлагаем старт в программировании – с нуля, без отрыва от занятий, без лишней траты времени, без ненужной академичности.";
         driver.findElement(By.cssSelector("#menu-item-10452")).click();
         driver.findElement(By.cssSelector("#post-9450")).click();
