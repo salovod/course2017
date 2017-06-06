f8abd563d2687890ccea451a817bbf9d4fd6df7b
LysechkoN <p12r11a19g92a>
2017-06-06 20:24:03 +0300

    add Recheck Test Basics of programming

--- src/test/java/mainAcad/MainAcad.java
+++ src/test/java/mainAcad/MainAcad.java
@@ -1,10 +1,11 @@
 package mainAcad;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import preparation.DriverConfiguration;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by gryzyuka on 06.06.2017.
@@ -15,7 +16,7 @@ public class MainAcad extends DriverConfiguration {
 
     @BeforeMethod
     public void beforeMethod() {
-        driver.manage().window().setSize(new Dimension(800, 600));
+//        driver.manage().window().setSize(new Dimension(800, 600));
         driver.get(basicURL);
     }
 
@@ -29,8 +30,11 @@ public class MainAcad extends DriverConfiguration {
 
     @Test
     public void Basic() throws InterruptedException {
+        String expectedText = "Колеблешься, ту ли специальность выбрал? Уверен в правильности выбора, но хочешь усилить свои позиции техническими знаниями?\nПредлагаем старт в программировании – с нуля, без отрыва от занятий, без лишней траты времени, без ненужной академичности.";
         driver.findElement(By.cssSelector("#menu-item-10452")).click();
         driver.findElement(By.cssSelector("#post-9450")).click();
+        String actualText = driver.findElement(By.cssSelector("#forWho > div.container > div > div:nth-child(1) > p")).getText();
+        assertEquals(actualText, expectedText);
 
 
     }
